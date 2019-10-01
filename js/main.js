8d88937507fa91f40894bb3565237b363e9f2c23
vovanich <vovanich_shkrobot@mail.ru>
2019-10-01 13:14:28 +0300

    hide burger-menu scroll

--- js/main.js
+++ js/main.js
@@ -4,9 +4,11 @@ document.getElementById("menuToggle").addEventListener("click", () => {
     menuOpened = false;
     document.getElementById("menu").style.transform = "";
     document.getElementById("menu").style.display = "none";
+    document.body.classList.remove("scroll-hide");
   } else {
     menuOpened = true;
     document.getElementById("menu").style.transform = "none";
     document.getElementById("menu").style.display = "block";
+    document.body.classList.add("scroll-hide");
   }
 });
